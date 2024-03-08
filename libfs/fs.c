f63020896037d0d4de05dcef62361a2dd0d27740
wbradford2001 <wbradford2001@gmail.com>
2024-03-07 16:41:34 -0800

    v

--- libfs/fs.c
+++ libfs/fs.c
@@ -857,7 +857,7 @@ int fs_read(int fd, void *buf, size_t count)
 	//get current fileDescriptor
 	struct fileDescriptor *curDescriptor = fileDescriptors[fd];
 
-	block_read(curDescriptor->dataIndices[0], buf);
+	block_read(superBlock.data_blk, buf);
 	// for (int i = 0; i < curDescriptor->numBlocks; i++){
 	// 	//read into buffer tempBuf
 	// 	void *tempBuf = malloc(BLOCK_SIZE * sizeof(int8_t));
