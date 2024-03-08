f610cad600fa8328947166e95518de85d445de8f
wbradford2001 <wbradford2001@gmail.com>
2024-03-07 18:15:03 -0800

    v

--- libfs/fs.c
+++ libfs/fs.c
@@ -766,7 +766,7 @@ int fs_write(int fd, void *buf, size_t count)
 		//update data indices
 		curDescriptor->dataIndices = realloc(curDescriptor->dataIndices, curDescriptor->numBlocks * sizeof(int));
 		curDescriptor->dataIndices[curDescriptor->numBlocks-1] = nextAvailableBlock;
-		
+		printf("%d\n", nextAvailableBlock);
 		//each time you write, increase the block that you write to
 		nextAvailableBlock++;
 
@@ -870,16 +870,16 @@ int fs_read(int fd, void *buf, size_t count)
 {
 	//get current fileDescriptor
 	struct fileDescriptor *curDescriptor = fileDescriptors[fd];
-	block_read(4, buf);
+	//block_read(4, buf);
 	//printf("read; %s\n", buf);
-	// for (int i = 0; i < curDescriptor->numBlocks; i++){
-	// 	//read into buffer tempBuf
-	// 	void *tempBuf = malloc(BLOCK_SIZE * sizeof(int8_t));
-	// 	block_read(curDescriptor->dataIndices[i], tempBuf);
+	for (int i = 0; i < curDescriptor->numBlocks; i++){
+		//read into buffer tempBuf
+		void *tempBuf = malloc(BLOCK_SIZE * sizeof(int8_t));
+		block_read(curDescriptor->dataIndices[i], tempBuf);
 
-	// 	//add tempBuf to but
-	// 	strcat(buf, tempBuf);
-	// }
+		//add tempBuf to but
+		strcat(buf, tempBuf);
+	}
 
 	return count;
 }
